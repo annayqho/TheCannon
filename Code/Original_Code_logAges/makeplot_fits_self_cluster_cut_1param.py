338ee8d97952ab71823de121305c53622314a5e5
Anna Ho <annayqho@gmail.com>
2014-10-28 16:03:51 +0100

    chisq

---
+++ Code/Original_Code_logAges/makeplot_fits_self_cluster_cut_1param.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python 
+import scipy 
+import numpy 
+import pickle
+from numpy import * 
+from scipy import ndimage
+from scipy import interpolate 
+from numpy import loadtxt
+import os 
+import numpy as np
+from numpy import * 
+import matplotlib 
+from pylab import rcParams
+from pylab import * 
+from matplotlib import pyplot
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from matplotlib.pyplot import axes
+from matplotlib.pyplot import colorbar
+#from matplotlib.ticker import NullFormatter
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+s = matplotlib.font_manager.FontProperties()
+s.set_family('serif')
+s.set_size(14)
+from matplotlib import rc
+rc('text', usetex=False)
+rc('font', family='serif')
+
+def plotfits():
+#    file_in = "self_tags.pickle"
+    file_in = "self_2nd_order_tags.pickle"
+    file_in2 = open(file_in, 'r') 
+    params, covs_params = pickle.load(file_in2)
+    sp = shape(params) 
+    params = array(params)
+    file_in2.close()
+
+    filein2 = 'test14.txt' 
+    filein3 = 'logAges.txt'
+ 
+    plot_markers = ['ko', 'yo', 'ro', 'bo', 'co','k*', 'y*', 'r*', 'b*', 'c*', 'ks', 'rs', 'bs', 'cs', 'rd', 'kd', 'bd', 'rd', 'mo', 'ms' ]
+    # M92, M15, M53, N5466, N4147, M13, M2, M3, M5, M107, M71, N2158, N2420, Pleaides, N7789, M67, N6819 , N188, N6791 
+    t,g,feh,t_err,feh_err = loadtxt(filein2, usecols = (4,6,8,16,17), unpack =1) 
+    tA,gA,fehA = loadtxt(filein2, usecols = (3,5,7), unpack =1) 
+    age = loadtxt(filein3, usecols = (0,), unpack =1) 
+    g_err = [0]*len(g) 
+    age_err = [0]*len(g) 
+    g_err = array(g_err)
+    diffT = abs(array(t) - array(tA) ) 
+    pick = diffT < 600. 
+    t,g,feh,t_err,g_err,feh_err = t[pick], g[pick], feh[pick], t_err[pick], g_err[pick], feh_err[pick] 
+    age = array(age)
+    age_err = array(age_err) 
+    age, age_err = age[pick] , age_err[pick] 
+    #
+    a = open(filein2) 
+    al = a.readlines() 
+    names = []
+    for each in al:
+      names.append(each.split()[1]) 
+    names = array(names) 
+    names = names[pick] 
+    unames = unique(names) 
+    starind = arange(0,len(names), 1) 
+    name_ind = [] 
+    names = array(names) 
+    for each in unames:
+      takeit = each == names 
+      name_ind.append(np.int(starind[takeit][-1]+1. ) )
+    cluster_ind = [0] + list(sort(name_ind))# + [len(al)]
+    #
+    
+    params = array(params)[pick]
+    covs_params = array(covs_params)[pick]
+    sp2 = shape(params) 
+    sp3 = len(t) 
+    #covs_params = np.linalg.inv(icovs_params) 
+    rcParams['figure.figsize'] = 12.0, 10.0
+    
+    fig, ax = pyplot.subplots()
+    ax.set_title("log(Age) Results from the Cannon")
+    #ax.set_ylabel("The Cannon", labelpad = 40, fontsize = 20 ) 
+    #ax.tick_params(labelcolor= 'w', top = 'on', bottom = 'on', left = 'off', right = 'off' ) 
+    params_labels = [params[:,0], params[:,1], params[:,2] , params[:,3], covs_params[:,0,0]**0.5, covs_params[:,1,1]**0.5, covs_params[:,2,2]**0.5 , covs_params[:,3,3]**0.5]
+    cval = ['k', 'b', 'r', 'c'] 
+    input_ASPCAP = [t, g, feh, age, t_err, g_err, feh_err, age_err] 
+    listit_1 = [0,1,2,3]
+    listit_2 = [1,0,0,0]
+    labels = ['teff', 'logg', 'Fe/H', 'age' ]
+    for i in range(0,len(cluster_ind)-1): 
+        indc1 = cluster_ind[i]
+        indc2 = cluster_ind[i+1]
+        pick = logical_and(g[indc1:indc2] > 0, logical_and(t_err[indc1:indc2] < 300, feh[indc1:indc2] > -4.0) ) 
+        ax.plot(input_ASPCAP[3][indc1:indc2][pick], params_labels[3][indc1:indc2][pick], plot_markers[i]) 
+    ax.text(10.0,7.0,"y-axis, $<\sigma>$ = "+str(round(mean(params_labels[3+4]),2)),fontsize = 14)
+
+    ax.plot([-5,25], [-5,25], linewidth = 1.5, color = 'k' ) 
+    ax.set_xlim(8.5, 10.5)  # for log ages
+    ax.set_ylim(8, 11) #for log ages 
+    ax.set_ylabel("log(Age [Gyr])", fontsize = 14,labelpad = 5) 
+    ax.set_xlabel("log(Literature Ages [Gyr])", fontsize = 14,labelpad = 10) 
+    prefix = "/home/annaho/AnnaCannon/test_self"
+    savefig(fig, prefix, transparent=False, bbox_inches='tight', pad_inches=0.5, orientation='landscape')
+    return 
+
+def savefig(fig, prefix, **kwargs):
+    for suffix in (".eps", ".png"):
+        print "writing %s" % (prefix + suffix)
+        fig.savefig(prefix + suffix, **kwargs)
+
+if __name__ == "__main__": #args in command line 
+    wl1,wl2,wl3,wl4,wl5,wl6 = 15392, 15697, 15958.8, 16208.6, 16120.4, 16169.5 
+    plotfits() 
+
---
+++ Code/Pleiades_CompareBrokenSpec.py
@@ -0,0 +1,51 @@
+import pyfits
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+import os
+
+base = "/home/annaho/AnnaCannon/Code/4259_Pleaides"
+allfiles = os.listdir('/home/annaho/AnnaCannon/Code/4259_Pleaides')
+allfiles.remove('cal_wget_ac_Pleaides.sh')
+allfiles.remove('Problems')
+
+def avg(data):
+    sum = data[0]
+
+    for i in range(1, len(data)):
+        sum = sum + data[i]
+
+    avg = sum/len(data)
+    return avg
+
+# We make the following spectrum: the average of all the stars that are not broken.
+brokenstars = ['aspcapStar-v304-2M03415868+2342263.fits', 'aspcapStar-v304-2M03422154+2439527.fits', 'aspcapStar-v304-2M03453903+2513279.fits', 'aspcapStar-v304-2M03463533+2324422.fits', 'aspcapStar-v304-2M03472083+2505124.fits', 'aspcapStar-v304-2M03473521+2532383.fits', 'aspcapStar-v304-2M03475973+2443528.fits', 'aspcapStar-v304-2M03482277+2358212.fits']
+
+specdata1 = []
+specdata2 = []
+
+for file in allfiles:
+    filename = "%s/%s" %(base, file)
+    a = pyfits.open(filename)
+    if file in brokenstars:
+        specdata1.append(np.array(a[1].data))
+    else:
+        specdata2.append(np.array(a[1].data))
+
+specdata1 = np.array(specdata1)
+specdata2 = np.array(specdata2)
+
+avgspec = avg(specdata2)
+
+colors = ['r-', 'r--', 'g-', 'g--', 'b-', 'b--', 'm-', 'm--']
+
+plt.plot(avgspec, 'k-', label="All Other Pleiades")
+
+for i in range(0, len(specdata1)):
+    dataset = specdata1[i]
+    plt.plot(dataset, colors[i], label=brokenstars[i])
+
+plt.title('Mean Spectrum for Each Group')
+plt.legend()
+plt.show()
+
