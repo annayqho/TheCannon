8720d541fd35fb1eaf9fcc341c5d0abc13f11789
Anna Ho <annayqho@gmail.com>
2015-01-14 13:53:15 +0100

    can construct training and test set for apogee

--- Code/Version1.0/test.py
+++ Code/Version1.0/test.py
@@ -4,19 +4,19 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
+from dataset import Dataset
 from aspcap import get_spectra
 from aspcap import continuum_normalize
 from aspcap import get_training_labels
 
 # CONSTRUCT TRAINING SET
 
-# Retrieve all training spectra
+# Retrieve all training IDs & spectra
 readin = "traininglabels.txt"
-filenames = np.loadtxt(readin, usecols=(0,), dtype='string', unpack=1)
-filenames1 = [] # for some reason if I try to replace the element,
-                # it gets rid of the '.fits' at the end...very annoying
-for i in range(0, len(filenames)): # incorporate file location info
-    filename = '/home/annaho/AnnaCannon/Data/APOGEE_Data' + filenames[i][1:]
+IDs = np.loadtxt(readin, usecols=(0,), dtype='string', unpack=1)
+filenames1 = [] # if I just replace the element, I lose the '.fits'
+for i in range(0, len(IDs)): # incorporate file location info
+    filename = '/home/annaho/AnnaCannon/Data/APOGEE_Data' + IDs[i][1:]
     filenames1.append(filename)
 spectra = get_spectra(filenames1)
 
@@ -31,26 +31,30 @@ colmask[cols] = 1
 label_names = [label_names[i] for i in cols]
 label_values = label_values[:,colmask]
 
-# 
+# Optional: Set desired stars
+Teff = label_values[:,0]
+Teff_corr = label_values[:,1]
+diff_t = np.abs(Teff-Teff_corr)
+diff_t_cut = 600.
+logg = label_values[:,2]
+logg_cut = 100.
+bad = np.logical_and((diff_t < diff_t_cut), logg < logg_cut)
+IDs = IDs[bad]
+spectra = spectra[bad]
+label_values = label_values[bad]
 
-trial_run = ReadASPCAP()
-training_set, to_discard = trial_run.set_star_set(True, training_label_names)
-test_set, nothing = trial_run.set_star_set(False, training_label_names)
+# Normalize the spectra
+normalized_spectra, continua = continuum_normalize(spectra)
 
-# Playing with the training_set object
-#IDs = training_set.get_IDs()
-#spectra = training_set.get_spectra()
-#spectra.shape # (523, 8575, 3)
-#pixels = spectra[:,:,0]
-#fluxes = spectra[:,:,1]
-#fluxerrs = spectra[:,:,2]
-#plot(pixels[0], fluxes[0])
-training_labels = training_set.get_label_values()
-#training_labels.shape # (523, 4)
-Teff, logg, FeH, age = training_labels[:,0], training_labels[:,1], training_labels[:,2], training_labels[:,3]
-#plt.hist(age) # plot the age distribution
+# Initialize the training set
+training_set = Dataset(IDs=IDs, spectra=spectra, label_names=label_names, label_values=label_values)
 
-# Run The Cannon
+# CONSTRUCT TEST SET
+
+# In this case, the training set and test set are the same
+test_set = Dataset(IDs=IDs, spectra=spectra, label_names=label_names)
+
+# STEP 1 OF THE CANNON: FIT FOR MODEL
 model = train_model(training_set)
 coeffs_all, covs, scatters, chis, chisqs, pivots = model
 coeffs_all.shape # (8575, 15)
@@ -58,7 +62,6 @@ cannon_labels, MCM_rotate, covs = infer_labels(nlabels, model, test_set)
 
 # Plot the results
 cannon_labels.shape # (553, 4)
-# Note that currently, there are more test stars than training stars because of the logg & Teff cuts. In order to compare the results, we need to perform the same filtering.
 filtered_cannon_labels = cannon_labels[to_discard]
 Cannon_Teff, Cannon_logg, Cannon_FeH, Cannon_age = filtered_cannon_labels[:,0], filtered_cannon_labels[:,1], filtered_cannon_labels[:,2], filtered_cannon_labels[:,3]
 plt.scatter(Teff, Cannon_Teff)
