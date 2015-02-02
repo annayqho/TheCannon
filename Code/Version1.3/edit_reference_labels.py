e8610c31e4f34dce31c63a4e932d03f6c3ecc05d
Anna Ho <annayqho@gmail.com>
2015-02-02 15:46:38 +0100

    delete extra column in some rows of label file

--- Code/Version1.3/edit_reference_labels.py
+++ Code/Version1.3/edit_reference_labels.py
@@ -1,13 +1,20 @@
-inputf = open("reference_labels.txt", "r")
-outputf = open("reference_labels_update.txt", "w")
+inputf = open("reference_labels_update.txt", "r")
+outputf = open("reference_labels.txt", "w")
 
 lines = inputf.readlines()
 outputf.write(lines[0])
 lines = lines[1:]
 
 for line in lines:
-    newline = line.split('/')[2]
-    outputf.write(newline)
+    labels = line.split('\t')
+    if len(labels) == 17:
+        outputf.write(line)
+    else:
+        trimmed = labels[0:len(labels)-1]
+        trimmed[-1] = trimmed[-1] + '\n'
+        outputf.write(trimmed[0])
+        for i in range(1, len(trimmed)):
+            outputf.write('\t' + trimmed[i])
 
 inputf.close()
 outputf.close()
