16d70f07ff81fab4b7ba24a1a08d9fdfcd9942ea
annayqho <annayqho@gmail.com>
2015-02-10 14:17:17 +0100

    delete Data folder

--- Data/APOGEE_Data/4217_N188/cal_wget_ac_N188.sh
+++
@@ -1,5 +0,0 @@
-wget --no-parent http://data.sdss3.org/sas/dr10/apogee/spectro/redux/r3/s3/a3/v304/4217/aspcapStar-v304-2M00283971+8530377.fits     
-wget --no-parent http://data.sdss3.org/sas/dr10/apogee/spectro/redux/r3/s3/a3/v304/4217/aspcapStar-v304-2M00454489+8504180.fits     
-wget --no-parent http://data.sdss3.org/sas/dr10/apogee/spectro/redux/r3/s3/a3/v304/4217/aspcapStar-v304-2M00320079+8511465.fits     
-wget --no-parent http://data.sdss3.org/sas/dr10/apogee/spectro/redux/r3/s3/a3/v304/4217/aspcapStar-v304-2M00463920+8523336.fits     
-wget --no-parent http://data.sdss3.org/sas/dr10/apogee/spectro/redux/r3/s3/a3/v304/4217/aspcapStar-v304-2M00533497+8511145.fits     
