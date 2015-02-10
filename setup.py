7f3a289a9481a99dcca6e5147f120124bd79cd72
annayqho <annayqho@gmail.com>
2015-02-10 14:34:40 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='thecannon',
+        version='0.1',
+        description='Data-driven stellar parameters and abundances from spectra',
+        url='http://github.com/annayqho/the-cannon',
+        author='Anna Ho',
+        author_email='annayqho@gmail.com',
+        license='MIT',
+        packages=['thecannon'],
+        zip_safe=False)
