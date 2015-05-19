a983c018e73dc2494476f8ee70927f37f14b630a
annayqho <annayqho@gmail.com>
2015-05-19 16:36:50 +0200

    try

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -105,7 +105,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -113,8 +113,8 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-print(sphinx_rtd_theme.get_html_theme_path())
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# print(sphinx_rtd_theme.get_html_theme_path())
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
