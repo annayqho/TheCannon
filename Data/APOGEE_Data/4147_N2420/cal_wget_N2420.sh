9f734d9f21146f8ea4f2d447696960c8b67a6ae6
annayqho <annayqho@gmail.com>
2015-05-19 13:26:59 +0200

    rebuild docs

--- Data/APOGEE_Data/4147_N2420/cal_wget_N2420.sh
+++
@@ -1,9 +0,0 @@
-wget --no-parent http://data.sdss3.org/sas/dr10/apogee/spectro/redux/r3/s3/a3/v304/4147/aspcapStar-v304-2M07380545+2136507.fits    
-wget --no-parent http://data.sdss3.org/sas/dr10/apogee/spectro/redux/r3/s3/a3/v304/4147/aspcapStar-v304-2M07381549+2138015.fits    
-wget --no-parent http://data.sdss3.org/sas/dr10/apogee/spectro/redux/r3/s3/a3/v304/4147/aspcapStar-v304-2M07382148+2135050.fits    
-wget --no-parent http://data.sdss3.org/sas/dr10/apogee/spectro/redux/r3/s3/a3/v304/4147/aspcapStar-v304-2M07382195+2135508.fits    
-wget --no-parent http://data.sdss3.org/sas/dr10/apogee/spectro/redux/r3/s3/a3/v304/4147/aspcapStar-v304-2M07382670+2128514.fits    
-wget --no-parent http://data.sdss3.org/sas/dr10/apogee/spectro/redux/r3/s3/a3/v304/4147/aspcapStar-v304-2M07382696+2138244.fits    
-wget --no-parent http://data.sdss3.org/sas/dr10/apogee/spectro/redux/r3/s3/a3/v304/4147/aspcapStar-v304-2M07382984+2134509.fits    
-wget --no-parent http://data.sdss3.org/sas/dr10/apogee/spectro/redux/r3/s3/a3/v304/4147/aspcapStar-v304-2M07380627+2136542.fits    
-wget --no-parent http://data.sdss3.org/sas/dr10/apogee/spectro/redux/r3/s3/a3/v304/4147/aspcapStar-v304-2M07381507+2134589.fits    
