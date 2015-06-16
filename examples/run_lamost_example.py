3e6bd46d031f53d90547b291037f0365ee6ed1a6
annayqho <annayqho@gmail.com>
2015-06-16 12:27:15 +0200

    minor

--- examples/run_lamost_example.py
+++ examples/run_lamost_example.py
@@ -1,15 +1,14 @@
-#from __future__ import (absolute_import, division, print_function)
-#from cannon.dataset import Dataset
+
 #from cannon.model import CannonModel
 #from cannon.spectral_model import draw_spectra, diagnostics, triangle_pixels, overlay_spectra, residuals
 #import numpy as np
-#import pickle
-#import random
+import pickle
+import random
 #import csv
 #from matplotlib import rc
 
-#plt.rc('text', usetex=True)
-#plt.rc('font', family='serif')
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 
 # STEP 1: DATA MUNGING
 import glob
@@ -25,11 +24,11 @@ label_file = "reference_labels.csv"
 tr_label = load_labels(label_file, tr_ID)
 test_IDs, wl, test_flux, test_ivar = load_spectra(dir_dat, test_ID)
 
-good = np.logical_and(dataset.tr_label[:,0] > 0, dataset.tr_label[:,2]>-5)
-tr_ID = tr_IDs[good]
-tr_flux = dataset.tr_flux[good]
-tr_ivar = dataset.tr_ivar[good]
-tr_label = dataset.tr_label[good]
+good = np.logical_and(tr_label[:,0] > 0, tr_label[:,2]>-5)
+tr_IDs = tr_IDs[good]
+tr_flux = tr_flux[good]
+tr_ivar = tr_ivar[good]
+tr_label = tr_label[good]
 
 from TheCannon import dataset
 dataset = dataset.Dataset(
@@ -45,7 +44,7 @@ dataset.diagnostics_ref_labels()
 # STEP 2: CONTINUUM IDENTIFICATION
 
 # Pseudo-continuum normalization for the training spectra
-if glob.glob('pseudo_normed_spec.p', 'r'):
+if glob.glob('pseudo_normed_spec.p'):
     (pseudo_tr_flux, pseudo_tr_ivar) = pickle.load(
             open("pseudo_normed_spec.p", "r"))
 
@@ -65,30 +64,37 @@ else:
     # Identify the best 5% of continuum pixels in each of the following
     # pixel regions 
     dataset.ranges = [[0,50], [50,100], [100,400], [400,600], [600,1722], [1863, 1950], [1950, 2500], [2500,3000], [3000, len(dataset.wl)]]
-    contmask = dataset.make_contmask(norm_tr_fluxes, norm_tr_ivars, frac=0.05)
+    contmask = dataset.make_contmask(pseudo_tr_flux, pseudo_tr_ivar, frac=0.05)
+    pickle.dump(contmask, open("contmask.p", "w"))
 
 dataset.set_continuum(contmask)
 
 
 # RUN CONTINUUM NORMALIZATION CODE
+dataset.ranges = [[0,1723], [1863,len(dataset.wl)]] # split into two wings
 
 if glob.glob('cont.p', 'r'):
-    tr_cont, test_cont = pickle.load(open("cont.p", "r"))
+    cont = pickle.load(open("cont.p", "r"))
 else:
-    dataset.ranges = [[0,1723], [1863,len(dataset.wl)]] # split into two wings
-    tr_cont, test_cont = dataset.fit_continuum(deg=3, ffunc="sinusoid")
-    pickle.dump((tr_cont, test_cont), open("cont.p", "w"))
+    cont = dataset.fit_continuum(deg=3, ffunc="sinusoid")
+    pickle.dump((cont), open("cont.p", "w"))
+
+# I want the cont for bad pixels to be simply the value, not 0
+tr_cont, test_cont = cont
+tr_cont[tr_cont==0] = dataset.tr_flux[tr_cont==0] 
+test_cont[test_cont==0] = dataset.test_flux[test_cont==0]
+cont = tr_cont, test_cont
 
 
 # Check out the median flux overlaid with cont pix 
 f_bar = np.zeros(len(dataset.wl))
 sigma_f = np.zeros(len(dataset.wl))
 for wl in range(0,len(dataset.wl)):
-    flux = norm_tr_fluxes[:,wl]
-    ivar = norm_tr_ivars[:,wl]
+    flux = pseudo_tr_flux[:,wl]
+    ivar = _tr_ivar[:,wl]
     f_bar[wl] = np.median(flux[ivar>0])
     sigma_f[wl] = np.sqrt(np.var(flux[ivar>0]))
-bad = np.var(norm_tr_ivars, axis=0) == 0
+bad = np.var(pseudo_tr_ivar, axis=0) == 0
 f_bar = np.ma.array(f_bar, mask=bad)
 sigma_f = np.ma.array(sigma_f, mask=bad)
 plot(dataset.wl, f_bar, alpha=0.7)
@@ -115,19 +121,20 @@ colorbar()
 
 
 # Plot the cont fits for 100 random training stars
+tr_cont, test_cont = cont
 pickstars = np.zeros(100)
 nstars = dataset.tr_flux.shape[0]
-for jj in range(100):
+for jj in range(10):
     pickstars[jj] = random.randrange(0, nstars-1)
 
 for jj in pickstars:
     #bad = np.var(norm_tr_ivars, axis=0) == 0
-    bad = norm_tr_ivars[jj,:] == 0 
-    flux = np.ma.array(dataset.tr_flux[jj,:], mask=bad)
+    bad = norm_tr_ivar[jj,:] == (1/200.)**2 
+    flux = np.ma.array(norm_tr_flux[jj,:], mask=bad)
     plot(dataset.wl, flux, alpha=0.7)
-    scatter(dataset.wl[contmask], flux[contmask], c='r')
-    cont = np.ma.array(tr_cont[jj,:], mask=bad)
-    plot(dataset.wl, cont)
+    #scatter(dataset.wl[contmask], flux[contmask], c='r')
+    #cont = np.ma.array(tr_cont[jj,:], mask=bad)
+    #plot(dataset.wl, cont)
     xlim(3800, 9100)
     title("Sample Continuum Fit")
     xlabel("Wavelength (A)")
@@ -135,40 +142,22 @@ for jj in pickstars:
     savefig('contfit_%s.png' %jj)
     plt.close()
 
-f,ax = plt.subplots(2, sharex=True)
-meanres = np.mean(res, axis=0)
-mu = mean(meanres)
-sig = np.sqrt(var(meanres))
-c= ax[1].imshow(sorted_res, cmap = plt.cm.bwr_r, interpolation='nearest',
-     vmin = mu-3*sig, vmax= mu+sig, aspect='auto')
-colorbar(c, orientation='horizontal')
-ax[0].plot(flux, alpha=0.7)
-ax[0].plot(cont)
-xlim(0,3626)
-
-ax[0].hist(meanres, bins = 60, range=(mu-3*sig, mu+sig))
-ax[0].set_ylabel("Raw Flux")
-title("Sample Raw Spec with All Continuum Fit Residuals")
-c = ax[1].imshow(res, cmap = plt.cm.bwr_r, interpolation='nearest',
-        vmin = -80, vmax=10, aspect='auto')
-colorbar(c, orientation='horizontal')
-ax[1].set_ylabel("Training Objects")
-
-
-norm_tr_fluxes, norm_tr_ivars, norm_test_fluxes, norm_test_ivars = \
-        dataset.continuum_normalize_f(cont=(tr_cont, test_cont))
+norm_tr_flux, norm_tr_ivar, norm_test_flux, norm_test_ivar = \
+        dataset.continuum_normalize(cont)
 
 
 # If you approve...
 
-dataset.tr_flux = norm_tr_fluxes
-dataset.tr_ivar = norm_tr_ivars
-dataset.test_flux = norm_test_fluxes
-dataset.test_ivar = norm_test_ivars
+dataset.tr_flux = norm_tr_flux
+dataset.tr_ivar = norm_tr_ivar
+dataset.test_flux = norm_test_flux
+dataset.test_ivar = norm_test_ivar
 
 # learn the model from the reference_set
-model = CannonModel(dataset, 3) # 2 = quadratic model
+from TheCannon import model
+model = model.CannonModel(dataset, 2) # 2 = quadratic model
 model.fit() # model.train would work equivalently.
+pickle.dump(coeffs_all, open("coeffs_all.p", "w"))
 
 # or...
 coeffs_all = pickle.load(open("coeffs_all.p", "r"))
@@ -181,7 +170,7 @@ if glob.glob('test_labels.p'):
     test_label = pickle.load(open('test_labels.p', 'r'))
     dataset.test_label = test_label
 else:
-    dataset, label_errs = model.infer_labels(dataset)
+    label_errs = model.infer_labels(dataset)
 
 # Make plots
 dataset.dataset_postdiagnostics(dataset)
