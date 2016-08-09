36c7a34d135aa91078ae02efe98b1396ce7a6ce8
Anna Ho <annayqho@gmail.com>
2016-08-09 17:35:55 +0200

    plot survey coverage

--- code/lamost/mass_age/cn/write_table.py
+++ code/lamost/mass_age/cn/write_table.py
@@ -1,85 +1,20 @@
 import numpy as np
+from astropy.table import Table, Column
+from astropy.io import ascii
 import glob
 
+DATA_DIR = "/Users/annaho/Data/Mass_And_Age"
+
 print("writing file")
-outfile = "lamost_catalog.csv"
-fout = open(outfile, "w")
+t = Table()
 
-files = np.array(glob.glob("*all.npz"))
-names = ['_'.join(item.split('_')[0:-1]) for item in files]
-header = ','.join(names)
-fout.write(header)
+files = np.array(glob.glob("%s/*all.npz" %DATA_DIR))
+for f in files:
+    fullname = f.split('/')[-1]
+    name = '_'.join(fullname.split('_')[0:-1]) 
+    vals = np.array(np.load(f)['arr_0'])
+    if name=='id':
+        vals = [(val.decode("utf-8")).split('/')[-1] for val in vals]
+    t[name] = Column(vals, description=name)
 
-id_test = np.load("id_all.npz")['arr_0']
-
-for i,id_val in enumerate(id_test):
-    vals =  np.array([np.load(f)['arr_0'][i] for f in files])
-    id_val = vals[files == 'id_all.npz'][0].decode("utf-8")
-    id_short = id_val.split("/")[-1]
-    vals[files == 'id_all.npz'] = id_short
-    vals = [val.decode("utf-8") for val in vals]
-    line = ','.join(vals) + '\n'
-    fout.write(line)
-
-fout.flush()
-fout.close()
-
-# print("loading test data")
-# teff_test = np.load("teff_all.npz")['arr_0']
-# logg_test = np.load("logg_all.npz")['arr_0']
-# feh_test = np.load("feh_all.npz")['arr_0']
-# cm_test = np.load("cm_all.npz")['arr_0']
-# nm_all = np.load("nm_all.npz")['arr_0']
-# alpha_test = np.load("alpha_all.npz")['arr_0']
-# 
-# # when a fit fails, I set the error to -9999
-# print("loading test errs")
-# teff_err_test = np.sqrt(np.load("teff_err_all.npz")['arr_0'])
-# logg_err_test = np.sqrt(np.load("logg_err_all.npz")['arr_0'])
-# feh_err_test = np.sqrt(np.load("feh_err_all.npz")['arr_0'])
-# cm_err_test = np.sqrt(np.load("cm_err_all.npz")['arr_0'])
-# nm_err_test = np.sqrt(np.load("nm_err_all.npz")['arr_0'])
-# alpha_err_test = np.sqrt(np.load("alpha_err_all.npz")['arr_0'])
-# chisq_test = np.load("chisq_all.npz")['arr_0']
-# 
-# npix_test = np.load("npix_all.npz")['arr_0']
-
-# add in the training set
-# print("loading training data")
-# direc = "../xcalib_5labels" 
-# direc1 = "/Users/annaho/TheCannon/data/lamost_paper"
-# id_training = np.load("%s/ref_id.npz" %direc1)['arr_0']
-# ngoodpix_training = np.sum(
-#         np.load("%s/ref_ivar.npz" %direc1)['arr_0'] > 0, axis=1)
-# label_training = np.load("%s/all_cannon_label_vals.npz" %direc)['arr_0']
-# err_training = np.load("%s/all_cannon_label_errs.npz" %direc)['arr_0']
-# chisq_training = np.load("%s/all_cannon_label_chisq.npz" %direc)['arr_0']
-# id_total = np.append(id_test, id_training)
-# teff_total = np.append(teff_test, label_training[:,0])
-# teff_err_total = np.append(teff_err_test, err_training[:,0])
-# logg_total = np.append(logg_test,label_training[:,1])
-# logg_err_total = np.append(logg_err_test,err_training[:,1])
-# feh_total = np.append(feh_test,label_training[:,2])
-# feh_err_total = np.append(feh_err_test,err_training[:,2])
-# alpha_total = np.append(alpha_test, label_training[:,3])
-# alpha_err_total = np.append(alpha_err_test, err_training[:,3])
-# ak_total = np.append(ak_test, label_training[:,4])
-# ak_err_total = np.append(ak_err_test,err_training[:,4])
-# npix_total = np.append(npix_test, ngoodpix_training)
-# chisq_total = np.append(chisq_test,chisq_training)
-# 
-# print("finding unique values")
-# id_all, inds_unique = np.unique(id_total, return_index=True)
-# teff = teff_total[inds_unique]
-# teff_err = teff_err_total[inds_unique]
-# logg = logg_total[inds_unique]
-# logg_err = logg_err_total[inds_unique]
-# feh = feh_total[inds_unique]
-# feh_err = feh_err_total[inds_unique]
-# alpha = alpha_total[inds_unique]
-# alpha_err = alpha_err_total[inds_unique]
-# ak = ak_total[inds_unique]
-# ak_err = ak_err_total[inds_unique]
-# npix = npix_total[inds_unique]
-# chisq_all = chisq_total[inds_unique]
- 
+t.write('lamost_catalog.csv', format='ascii.fast_csv')
--- code/lamost/mass_age/paper_plots/plot_survey_coverage.py
+++ code/lamost/mass_age/paper_plots/plot_survey_coverage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 import healpy as hp
+import astropy.table as Table
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib import rc
@@ -11,30 +12,32 @@ plt.rc('font', family='serif')
 import pyfits
 
 # import the data
-hdulist = pyfits.open("../cn/lamost_catalog_full.fits")
+hdulist = pyfits.open(
+"/Users/annaho/Data/Mass_And_Age/lamost_catalog_mass_age.fits")
 tbdata = hdulist[1].data
-# cols = hdulist[1].columns
-# cols.names
+# # cols = hdulist[1].columns
+# # cols.names
 ra_lamost = tbdata.field('ra')
 dec_lamost = tbdata.field('dec')
-am_lamost = tbdata.field("cannon_a_k")
-rmag_lamost = tbdata.field("mag3")
+val_lamost = tbdata.field("age")
 hdulist.close()
-hdulist = pyfits.open("/home/annaho/aida41040/annaho/TheCannon/examples/example_DR12/allStar-v603.fits")
+
+hdulist = pyfits.open(
+    "/Users/annaho/Data/APOGEE/Ness2016_Catalog_Full_DR12_Info.fits")
 tbdata = hdulist[1].data
+
 ra_apogee_all = tbdata['RA']
 dec_apogee_all = tbdata['DEC']
-am_apogee_all = tbdata['PARAM_ALPHA_M']
-am_apogee_all = tbdata['AK_WISE']
+val_apogee_all = np.exp(tbdata['lnAge'])
 good_coords = np.logical_and(ra_apogee_all > -90, dec_apogee_all > -90)
-good = np.logical_and(good_coords, am_apogee_all > -90)
+good = np.logical_and(good_coords, val_apogee_all > -90)
 ra_apogee = ra_apogee_all[good]
 dec_apogee = dec_apogee_all[good]
-am_apogee = am_apogee_all[good]
+val_apogee = val_apogee_all[good]
 hdulist.close()
 ra_both = np.hstack((ra_apogee, ra_lamost))
 dec_both = np.hstack((dec_apogee, dec_lamost))
-am_all = np.hstack((am_apogee, am_lamost))
+val_all = np.hstack((val_apogee, val_lamost))
 
 # create a RA and Dec grid
 ra_all = []
@@ -84,11 +87,11 @@ mask_lamost = np.zeros(hp.nside2npix(NSIDE), dtype='bool')
 for pix_val in np.unique(pix_lamost):
     choose = np.where(pix_lamost==pix_val)[0]
     if len(choose) == 1:
-        #m_lamost[pix_val] = rmag_lamost[choose[0]]
-        m_lamost[pix_val] = am_lamost[choose[0]]
+#         #m_lamost[pix_val] = rmag_lamost[choose[0]]
+        m_lamost[pix_val] = val_lamost[choose[0]]
     else:
         #m_lamost[pix_val] = np.median(rmag_lamost[choose])
-        m_lamost[pix_val] = np.median(am_lamost[choose])
+        m_lamost[pix_val] = np.median(val_lamost[choose])
 
 mask_lamost[np.setdiff1d(np.arange(len(m_lamost)), pix_lamost)] = 1
 m_lamost.mask = mask_lamost
@@ -99,9 +102,9 @@ mask_apogee= np.zeros(hp.nside2npix(NSIDE), dtype='bool')
 for pix_val in np.unique(pix_apogee):
     choose = np.where(pix_apogee==pix_val)[0]
     if len(choose) == 1:
-        m_apogee[pix_val] = am_apogee[choose[0]]
+        m_apogee[pix_val] = val_apogee[choose[0]]
     else:
-        m_apogee[pix_val] = np.median(am_apogee[choose])
+        m_apogee[pix_val] = np.median(val_apogee[choose])
 
 mask_apogee[np.setdiff1d(np.arange(len(m_apogee)), pix_apogee)] = 1
 m_apogee.mask = mask_apogee
@@ -112,9 +115,9 @@ mask_all= np.zeros(hp.nside2npix(NSIDE), dtype='bool')
 for pix_val in np.unique(pix_all):
     choose = np.where(pix_all==pix_val)[0]
     if len(choose) == 1:
-        m_all[pix_val] = am_all[choose[0]]
+        m_all[pix_val] = val_all[choose[0]]
     else:
-        m_all[pix_val] = np.median(am_all[choose])
+        m_all[pix_val] = np.median(val_all[choose])
 
 mask_all[np.setdiff1d(np.arange(len(m_all)), pix_all)] = 1
 m_all.mask = mask_all
@@ -126,21 +129,21 @@ cmap.set_under('w')
 # composite map
 # plot map ('C' means the input coordinates were in the equatorial system)
 rcParams.update({'font.size':16})
-#hp.visufunc.mollview(m_apogee, coord=['C','G'], rot=(150, 0, 0), flip='astro',
-#        notext=True, title=r'$\alpha$/M for APOGEE DR12', cbar=True,
-#        norm=None, min=-0.07, max=0.3, cmap=cmap, unit = r'$\alpha$/M [dex]')
+# hp.visufunc.mollview(m_apogee, coord=['C','G'], rot=(150, 0, 0), flip='astro',
+#         notext=True, title=r'Age for Ness et al. 2016', cbar=True,
+#         norm=None, min=0, max=12, cmap=cmap, unit = 'Gyr')
 #hp.visufunc.mollview(m_lamost, coord=['C','G'], rot=(150, 0, 0), flip='astro',
 #        notext=True, title=r'$\alpha$/M for 500,000 LAMOST giants', cbar=True,
 #        norm=None, min=-0.07, max=0.3, cmap=cmap, unit = r'$\alpha$/M [dex]')
         #notext=True, title="r-band magnitude for 500,000 LAMOST giants", cbar=True,
         #norm=None, min=11, max=17, cmap=cmap, unit = r"r-band magnitude [mag]")
 hp.visufunc.mollview(m_all, coord=['C','G'], rot=(150, 0, 0), flip='astro',
-        notext=True, title=r'$\alpha$/M for APOGEE DR12 + 500,000 LAMOST giants', cbar=True,
-        norm=None, min=0.00, max=0.4, cmap=cmap, unit = r'$\alpha$/M [dex]')
+        notext=True, title='Age from Ness et al. 2016 + LAMOST giants', 
+        cbar=True, norm=None, min=0.00, max=12, cmap=cmap, unit = 'Gyr')
 hp.visufunc.graticule()
 
 #plt.show()
-plt.savefig("full_ak_map.png")
-#plt.savefig("apogee_am_map.png")
+plt.savefig("full_age_map.png")
+#plt.savefig("apogee_age_map.png")
 #plt.savefig("lamost_am_map_magma.png")
 #plt.savefig("lamost_rmag_map.png")
