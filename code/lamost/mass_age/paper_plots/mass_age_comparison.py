08e9b09a9fa6c84c217087bab900321f0df6b551
Anna Ho <annayqho@gmail.com>
2016-09-01 11:17:43 -0700

    all on log scale

---
+++ code/lamost/mass_age/paper_plots/mass_age_comparison.py
@@ -0,0 +1,59 @@
+import pyfits
+from matplotlib import rc
+from matplotlib.colors import LogNorm
+import matplotlib.pyplot as plt
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+import numpy as np
+import sys
+import matplotlib.gridspec as gridspec
+
+def load_comparison():
+    direc = "/Users/annaho/Data/LAMOST/Mass_And_Age"
+    hdulist = pyfits.open("%s/age_vs_age_catalog.fits" %direc)
+    tbdata = hdulist[1].data
+    hdulist.close()
+    snr = tbdata.field("snr")
+    mass_raw = tbdata.field("cannon_mass")
+    choose = np.logical_and(snr > 0, mass_raw > 0)
+    mass = np.log10(tbdata.field("cannon_mass")[choose])
+    age = tbdata.field("cannon_age")[choose]
+    age_err = tbdata.field("cannon_age_err")[choose]
+    mass_err = tbdata.field("cannon_mass_err")[choose]
+    ness_age = np.log10(np.exp(tbdata.field("lnAge")[choose]))
+    ness_age_err = tbdata.field("e_logAge")[choose]
+    ness_mass = np.log10(np.exp(tbdata.field("lnM")[choose]))
+    ness_mass_err = tbdata.field("e_logM")[choose]
+    return mass, age, ness_mass, ness_age
+
+
+def plot(ax, x, y, xlabel, ylabel, axmin, axmax):
+    a,b,c, im = ax.hist2d(
+            x, y,
+            bins=50, norm=LogNorm(), cmap="gray_r")
+    cbar = plt.colorbar(im, ax=ax, orientation='horizontal')
+    cbar.set_label("Density", fontsize=20)
+    cbar.ax.tick_params(labelsize=20)
+    ax.plot([axmin,axmax], [axmin,axmax], c='k')
+    ax.set_xlabel(xlabel, fontsize=20)
+    ax.set_ylabel(ylabel, fontsize=20)
+    ax.tick_params(axis='y', labelsize=20)
+    ax.tick_params(axis='x', labelsize=20)
+    ax.set_xlim(axmin, axmax)
+    ax.set_ylim(axmin, axmax)
+
+
+if __name__=="__main__":
+    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12,6))
+    mass, age, ness_mass, ness_age = load_comparison()
+    plot(
+            ax1, ness_mass, mass,
+            "log(Mass) from APOGEE", "log(Mass) from LAMOST C and N",
+            -0.15, 0.25)
+    plot(
+            ax2, ness_age, age, 
+            "log(Age) from APOGEE Masses", "log(Age) from LAMOST C and N",
+            -0.5,1.5)
+    plt.tight_layout()
+    #plt.savefig("age_density.png")
+    plt.show()
