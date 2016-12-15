3b15f7c71e4404f6022c7968e020fc1cf7215354
Anna Ho <annayqho@gmail.com>
2016-12-15 16:05:37 +0900

    open files

--- code/lamost/li_giants/compile_candidates.py
+++ code/lamost/li_giants/compile_candidates.py
@@ -1,3 +1,11 @@
 """ Compile candidates from the individual candidates txt files """
+import os
+import glob
 
+direc = "/Users/annaho/Data/Li_Giants/All_Candidates"
 
+files = glob.glob(direc + "/*.txt")
+outf = open(direc + "/all_candidates.txt")
+for f in files:
+    candidates = np.loadtxt(f)
+    
