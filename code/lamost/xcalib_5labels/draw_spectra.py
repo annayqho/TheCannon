dba9647faab09f9ab959b6c36005aac3310ed65c
Anna Ho <annayqho@gmail.com>
2017-02-10 10:17:04 -0800

    rebuilt docs

--- code/lamost/xcalib_5labels/draw_spectra.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-coeffs = np.load("coeffs.npz")['arr_0']
-chisqs = np.load("chisqs.npz")['arr_0']
-pivots = np.load("pivots.npz")['arr_0']
-scatters = np.load("scatters.npz")['arr_0']
