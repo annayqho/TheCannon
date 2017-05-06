7a8ce550af0314fdf98926687c47ae03cb32e080
Anna Ho <annayqho@gmail.com>
2017-05-06 16:44:00 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='TheCannon',
-        version='0.6.0',
+        version='0.6.2',
         description='Data-driven stellar parameters and abundances from spectra',
         url='http://github.com/annayqho/TheCannon',
         author='Anna Y. Q. Ho',
